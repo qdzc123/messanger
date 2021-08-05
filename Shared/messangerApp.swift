2360499a31875ff69a3141431befa7c9c096df9c
Hosung <zmffhqk@gmail.com>
2021-08-05 13:27:06 -0500

    Initial Commit

---
+++ Shared/messangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  messangerApp.swift
+//  Shared
+//
+//  Created by Hosung Lee on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct messangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
